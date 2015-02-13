8fe8deb98e4d0f9ab81a960b3de10d6e37dc67cb
Mike Stewart <stewart@MediaDoneRight.com>
2015-02-13 00:35:22 -0800

    Setup compass with typical config.rb file

---
+++ compass.rb
@@ -0,0 +1,30 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+#http_path = "/"
+#project_path = "/"
+css_dir = "_site"
+sass_dir = "_scss"
+#sass_path = "_scss"
+images_dir      = "images"
+javascripts_dir = "js"
+
+output_style = :expanded
+environment = :development
+sourcemap = true
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+#sass_options = (environment == :development && sourcemap == true) ? {:debug_info => true} : {}
